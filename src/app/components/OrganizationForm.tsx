dbbfa6728352f427347c4d1486b555e9455c5ded
AstoriaBlack-botcalm <kithmini.j@botcalm.com>
2026-01-12 10:10:48 +0530

    feat: Update OrganizationForm component to include new validation rules

--- src/app/components/OrganizationForm.tsx
+++ src/app/components/OrganizationForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 import { Organization, SubAdmin } from '../page';
 import { Save, X, UserPlus } from 'lucide-react';
